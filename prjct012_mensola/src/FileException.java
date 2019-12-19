06970cdb61ac693d7d5e22c87058b91b65b37b50
IsoardoJonathan <57211636+IsoardoJonathan@users.noreply.github.com>
2019-12-19 08:38:13 +0100

    Add files via upload

---
+++ prjct012_mensola/src/FileException.java
@@ -0,0 +1,15 @@
+public class FileException extends Exception {
+    private String matter ="";
+
+    public FileException(String matter) {
+        this.matter = matter;
+    }
+
+    public String getMatter() {
+        return matter;
+    }
+
+    public void setMatter(String matter) {
+        this.matter = matter;
+    }
+}
